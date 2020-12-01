21cbd990a0aad32c410b9723e1760ffc7611a1d9
Topzagz <65101147+topzagz@users.noreply.github.com>
2020-12-01 11:43:29 +0700

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ class RootIndex extends React.Component {
 
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#000' }}>
+        <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
           <div className="wrapper">
